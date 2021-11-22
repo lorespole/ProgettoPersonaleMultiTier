56e7f0df90ab3d511ede10eb7a8780f5c62d72af
LAPTOP-GLOUD29L\spoly <lorenzo.spoletini77@gmail.com>
2021-11-22 16:12:22 +0100

    implementazione base lista prodotti con foreach

--- Entity/Products.cs
+++ Entity/Products.cs
@@ -10,6 +10,7 @@ namespace ProgettoPersonaleMultiTier.Entity
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
+        public Categories CategoryName { get; set; }
         public int? SupplierId { get; set; }
         public int? CategoryId { get; set; }
         public string QuantityPerUnit { get; set; }
--- Models/ProdottoViewModel.cs
+++ Models/ProdottoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgettoPersonaleMultiTier.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace ProgettoPersonaleMultiTier.Models
         public string ProductName { get; set; }
         public int? SupplierId { get; set; }
         public int CategoryId { get; set; }
+        public Categories CategoryName { get; set; }
+
         public string QuantityPerUnit { get; set; }
         public decimal? UnitPrice { get; set; }
         public short? UnitsInStock { get; set; }
